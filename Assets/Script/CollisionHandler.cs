749eb8015f5c260b5290f351e08ed8bf7d0cf56d
Tristan Newball <ninjatrisboy@gmail.com>
2022-06-02 10:10:27 -0500

    Added Tags so we know if its Blown Up, Win, or Start.

---
+++ Assets/Script/CollisionHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("Congrats, you finished");
+                break;
+            case "Fuel":
+                Debug.Log("You got fuel");
+                break;
+            default:
+                Debug.Log("Sorry, you blew up");
+                break;
+
+        }
+    }
+}
